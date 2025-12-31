6c9dae1d07f52318fad1d9379b95e30e81a74fc1
dhtt <denghtcarrie@gmail.com>
2025-12-31 19:10:46 +0800

    finally

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import time
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 # 配置Socket.IO支持CORS
-socketio = SocketIO(app, cors_allowed_origins='*')
+socketio = SocketIO(app, cors_allowed_origins='*',async_mode='threading')
 
 # 添加设备连接管理
 connected_devices = {}
